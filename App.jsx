fcd3010f22c9dcaf142bc9dc2c9c0739a2a9ba9f
Manpreet kaur <155741403+Mankaurjassl@users.noreply.github.com>
2025-05-31 19:14:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,43 @@
+// import Hello from './components/Hello'
+// import Bye from './components/Bye'
+// import Testing from './components/Testing'
+// import Props from './components/Props'
+// import Array_props from './components/Array_props'
+
+// import Object_render from './components/Object_render'
+
+// import Event_handler from './components/Event_handler'
+// import UseState_inc_de from './components/UseState_inc_dec'
+// import Form from "./components/Form"
+
+import TO_do_app from './components/TO_do_app'
+
+function App() {
+  return (
+    <>
+     <TO_do_app/>
+    {/* <Form/>*/}
+    
+      {/* <Hello />
+      <Bye />
+      <Testing />
+      <Props name="manpreet" phone="1234567890" /> */} 
+
+       {/* <Array_props/> */}
+
+
+    {/* <Object_render/> */}
+  
+
+ { /*<Event_handler/> */}
+      
+      {/* <UseState_inc_de/> */}
+
+         
+      
+
+    </>
+  )
+}
+
+export default App
